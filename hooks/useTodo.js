21eb37c06af6d79f1de2f6d0d1485d08b372fc41
Alex R <alexis.rj2110@gmail.com>
2025-04-27 21:54:37 -0500

    V1

---
+++ hooks/useTodo.js
@@ -0,0 +1,51 @@
+import {useEffect, useReducer, useState} from "react";
+import {todoReducer} from "../08-useReducer/todoReducer.js";
+
+const init = () => {
+    return JSON.parse(localStorage.getItem("toDos")) || [];
+}
+
+export const useTodo = () => {
+
+    const [toDos, dispatch] = useReducer(todoReducer, [], init);
+
+    useEffect(() => {
+
+        localStorage.setItem("toDos", JSON.stringify(toDos));
+
+    }, [toDos])
+
+    const handleNewTodo = (todo) => {
+
+        const action = {
+            type: "ADD_TODO",
+            payload: todo
+        }
+
+        dispatch(action);
+    }
+
+    const handleDeleteTodo = (id) => {
+        dispatch({
+            type: "REMOVE_TODO",
+            payload: id,
+        });
+    }
+
+    const handleToggleTodo = (id) => {
+        dispatch({
+            type: "TOGGLE_TODO",
+            payload: id,
+        })
+    }
+
+    return {
+        toDos,
+        toDosCount: toDos.length,
+        pendingToDosCount: toDos.filter(todo => !todo.done).length,
+        handleNewTodo,
+        handleDeleteTodo,
+        handleToggleTodo,
+    }
+
+}
